54c73f71d6d43a0cfac20817319329dd09007ba3
Egor Toropov <edoch@yandex.ru>
2025-06-10 11:06:48 +0300

    add folder styles and create global.module.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "normalize.css";
+import "./styles/global.module.scss";
 import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
